01f1f1bd7cfe57e545d81e45d2cd6095725fcdde
GoffetteDorian <goffettedorian@gmail.com>
2020-05-18 12:11:33 +0200

    Basic setup discord.js bot application

---
+++ src/index.js
@@ -0,0 +1,8 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.once("ready", () => {
+    console.log("Ready!");
+});
+
+client.login("NzExODgwNjQyMzg3NzcxNDMz.XsJcug.xGvToH0A1jMHr3rR5RecGbAlTmw");
