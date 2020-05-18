31060bdb4dd3d5ace964e2b72d6507d3bc71e5d1
GoffetteDorian <goffettedorian@gmail.com>
2020-05-18 14:49:42 +0200

    Some testing of the discord api

--- src/index.js
+++ src/index.js
@@ -1,8 +1,43 @@
 const Discord = require("discord.js");
+const fs = require("fs");
+const {prefix, token} = require("../config.json");
+const {players} = require("./assets/players.json");
+
 const client = new Discord.Client();
 
 client.once("ready", () => {
-    console.log("Ready!");
+    console.log("rdy!");
 });
 
-client.login("NzExODgwNjQyMzg3NzcxNDMz.XsJcug.xGvToH0A1jMHr3rR5RecGbAlTmw");
+client.on("message", (message) => {
+    if (message === "!h") {
+        message.channel.send("HELP");
+    }
+});
+
+client.on("message", (message) => {
+    if (!message.content.startsWith(prefix) || message.author.bot) {
+        return;
+    }
+    const args = message.content.slice(prefix.length).split(" ");
+    const command = args.shift().toLowerCase();
+
+    if (command === "set-character") {
+        if (!args.length) {
+            return message.channel.send("Missing arguments!");
+        }
+        fs.writeFile(
+            "./assets/players.json",
+            JSON.stringify({...args}),
+            (err) => {
+                if (err) {
+                    console.error(err);
+                }
+            },
+        );
+        console.log();
+        message.channel.send(`Character for ${message.author}`);
+    }
+});
+
+client.login(token);
